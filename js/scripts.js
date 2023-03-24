fa2f7d7154d1d204675ceb266e42b9e6f30626e4
Eusebie Siebenberg <siebenee@gmail.com>
2023-03-23 17:15:42 -0700

    Fix indention on scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -6,28 +6,25 @@ function buildCharacter(event) {
   const species = document.getElementById("species").value;
 
   let result;
-    if (age === "old") {
-      if (species === "elf") {
-      result = "old elf"; 
-    }
-    else if (species === "human") {
+  if (age === "old") {
+    if (species === "elf") {
+      result = "old elf";
+    } else if (species === "human") {
       result = "old human";
     }
-  }
-    else if (age === "young") {
-      if (species === "elf") {
+  } else if (age === "young") {
+    if (species === "elf") {
       result = "young elf";
-      }
-    else if (species === "human") {
+    } else if (species === "human") {
       result = "young human";
     }
-    }
+  }
 
   document.querySelector("p#results").innerText = "Your character: " + " " + name + " " + "the " + result;
   document.querySelector("p#results").removeAttribute("class");
 }
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   const form = document.getElementById("character");
   form.addEventListener("submit", buildCharacter);
 });
